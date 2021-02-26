e6d6ddde08809a81f4c455b84a80e589ff27152c
jcoriell <47751211+jcoriell@users.noreply.github.com>
2021-02-26 09:53:27 -0600

    Update main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,6 +1,6 @@
 
 //% weight=100 color=#0fbc11 icon=""
-namespace cyberbot {
+namespace oop {
     
     //%block="pin %p"
     export function newPin(p: number): Pin{
@@ -59,15 +59,17 @@ namespace cyberbot {
             control.reset();
         }
 
-        private send_c(c:number, s=0, d:any=null, f:any=null): void{
+        private send_c(c:number, s=0, d:number=null, f:number=null): void{
             let a = Buffer.fromArray([1, this.pA, this.pB, s]);
             if (d !== null){
                 let b = Buffer.fromArray([Math.round(d)]);
-                a.concat(b);
+                Buffer.concat([a,b]);
+                //a.concat(b);
             }
             if (f !== null){
                 let b = Buffer.fromArray([Math.round(f)]);
-                a.concat(b);
+                Buffer.concat([a,b]);
+                //a.concat(b);
             }         
             try{
                 pins.i2cWriteBuffer(93, a)
@@ -99,25 +101,89 @@ namespace cyberbot {
         }
 
 
-    }
+    }  
+}
+
+
+namespace functional{
     
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
     //% block
-    export function foo(n: number,): void {
-        // Add code here
+    export function connect(){
+        while (true) {​​​​​
+            try {​​​​​
+                pins.i2cReadBuffer(93, 1)
+            }​​​​​ 
+            catch (error) {​​​​​
+                serial.writeLine("Error:");
+                serial.writeLine(error);
+                continue;
+            }​​​​​
+            finally {​​​​​
+                pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+                basic.pause(10)
+                let mybuff = Buffer.fromArray([12])
+                pins.i2cWriteBuffer(93, mybuff)
+                basic.pause(10)
+                while (true) {​​​​​
+                    try {​​​​​
+                        let i2cBuffer = pins.i2cReadBuffer(93, 1,false)
+                        serial.writeLine("NUMBER:")
+                        serial.writeNumber(i2cBuffer.getNumber(NumberFormat.Int8LE, 0))
+                    }​​​​​
+                    catch (error){​​​​​
+                        serial.writeLine("Error2:");
+                        serial.writeLine(error);
+                        continue;
+                    }​​​​​
+                    finally {​​​​​
+                        break
+                    }​​​​​
+                }​​​​​
+                break
+            }​​​​​
+        }​​​​​
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+    function botDisable(): void{
+            pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+            basic.pause(200);
+            control.reset();
+        }
+
+
+    function send_c(p: number, c:number, q=33, s=0, d:number=null, f:number=null): void{
+            let a = Buffer.fromArray([1, p, q, s]);
+            if (d !== null){
+                let b = Buffer.fromArray([Math.round(d)]);
+                Buffer.concat([a,b]);
+                //a.concat(b);
+            }
+            if (f !== null){
+                let b = Buffer.fromArray([Math.round(f)]);
+                Buffer.concat([a,b]);
+                //a.concat(b);
+            }         
+            try{
+                pins.i2cWriteBuffer(93, a)
+                let e = Buffer.fromArray([0,c])
+                pins.i2cWriteBuffer(93, e)
+                c = 0x01
+                while ( c !== 0){
+                    let b = Buffer.fromArray([0x00])
+                    pins.i2cWriteBuffer(93, b)
+                    c = pins.i2cReadNumber(93, NumberFormat.Int8LE)
+                }
+            }
+            catch{
+                botDisable()
+            }
+
+        }
+
+        //% block="%p write digital %s"
+        export function write_digital(p: number, s: number): void{
+            if (s > 1 || s < 0){s = 4} 
+            else if (s === 0){s = 2}
+            send_c(p, s)
+        }
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-basic.forever(function () {
-    cyberbot.newPin(22).tone(2000, 2000)
-})
+functional.connect()
+functional.write_digital(20, 1)
+functional.write_digital(21, 1)
