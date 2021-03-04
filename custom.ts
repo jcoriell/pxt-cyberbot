aea65a6fec05b3f5fb1c8bbd91a622a8025f13d0
jcoriell <47751211+jcoriell@users.noreply.github.com>
2021-03-04 16:49:49 -0600

    Update main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,39 +1,32 @@
 
 
-namespace functional{
+namespace cyberbot{
+
     
     //% block
     export function connect(){
         while (true) {
-            if (pins.i2cReadNumber(93, NumberFormat.UInt16LE) != 0) {
-                pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+            if (pins.i2cReadNumber(93, NumberFormat.UInt16LE) !== 0){
+                pins.digitalWritePin(DigitalPin.P8, 1)
                 pause(10)
                 pins.i2cWriteNumber(93, 12, NumberFormat.UInt16LE)
                 pause(10)
-                while (true) {
-                    if (pins.i2cReadNumber(93, NumberFormat.UInt16LE) != 0) {
-                        console.log("second reading num: " + pins.i2cReadNumber(93, NumberFormat.UInt16LE))
-                        console.log("second reading hex:" + pins.i2cReadBuffer(93, 8).toHex())
-                        break
-                    }
-                }
                 break
-                pause(10)
             }
         }
     }
+
 
     function botDisable(): void{
             pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
             basic.pause(200);
             control.reset();
-            console.log("disabled")
         }
 
 
     function send_c(p: number, c:number, q=33, s=0, d:number=null, f:number=null): void{
+            // build args and write
             let args = Buffer.fromArray([1, p, q, s]);
-            console.log("args = " + args.toHex())
             if (d !== null){
                 let duration = pins.createBuffer(4)
                 duration.setNumber(NumberFormat.UInt16LE, 0, Math.round(d))
@@ -43,31 +36,19 @@ namespace functional{
                 let freq = pins.createBuffer(4)
                 freq.setNumber(NumberFormat.UInt16LE, 0, Math.round(f))
                 args = Buffer.concat([args,freq]);
-            }      
-            console.log("allargs = " + args.toHex())
-            pins.i2cWriteBuffer(93, args) 
-            pause(10)
-            console.log("args written")
-            
-            let command = pins.createBuffer(2)
-            command.setNumber(NumberFormat.UInt8LE, 1, c)
-            console.log("command = " + command.toHex())
-            pins.i2cWriteBuffer(93, command)
-            pause(10)
-            console.log("command written")
-            let read = pins.i2cReadBuffer(93, 4).toHex()
-            console.log("Read is " + read)    
-            let check = '0001'
-            while ( check !== '0000'){
-                let b = Buffer.fromHex('0000')
-                pins.i2cWriteBuffer(93, b)
-                pause(10)
-                console.log("wrote b as " + b.toHex())
-                check = pins.i2cReadBuffer(93, 2).toHex()
-                console.log("check is " + check)
             }   
+            pins.i2cWriteBuffer(93, args);
+            
+            // build command and write
+            let command = Buffer.fromArray([0,c]);
+            pins.i2cWriteBuffer(93, command);
 
-            console.log('---------------------------------')
+            // wait until prop is done
+            let check = 1
+            while ( check !== 0){
+                pins.i2cWriteNumber(93, 0, NumberFormat.UInt8LE);
+                check = pins.i2cReadNumber(93, NumberFormat.UInt8LE);
+            }   
 
         }
 
@@ -75,9 +56,7 @@ namespace functional{
         export function write_digital(p: number, s: number): void{
             if (s > 1 || s < 0){s = 4} 
             else if (s === 0){s = 2}
-            console.log("Writing digital on " + p + "with a value of " + s)
             send_c(p, s)
-            console.log("Done sending command.")
         }
 
         //% block="%pin tone freq %f dur %d "
--- main.ts
+++ main.ts
@@ -1,23 +1,11 @@
 basic.forever(function () {
-    // on
-    pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
-    
-    pins.i2cWriteBuffer(0X5D, Buffer.fromHex('01142100'))
-    pins.i2cWriteBuffer(0X5D, Buffer.fromHex('0001'))
+    cyberbot.connect()
+    cyberbot.write_digital(20, 1)
     basic.pause(500)
-
-    pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-    basic.pause(200)
-    
-
-    // off
-    pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
-
-    pins.i2cWriteBuffer(0X5D, Buffer.fromHex('01142100'))
-    pins.i2cWriteBuffer(0X5D, Buffer.fromHex('0002'))
+    cyberbot.write_digital(20, 0)
     basic.pause(500)
-
-    pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-    basic.pause(200)
-    control.reset()
+    cyberbot.write_digital(21, 1)
+    basic.pause(500)
+    cyberbot.write_digital(21, 0)
+    basic.pause(500)
 })
