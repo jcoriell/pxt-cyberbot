295ff0e041ed9e2d523577790239884cc86f302c
jcoriell <47751211+jcoriell@users.noreply.github.com>
2021-03-12 11:07:10 -0600

    Update pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,9 +1,11 @@
 
 namespace cyberbot{
 
-    let program1: () => void;
-    let program2: () => void;
-    let program3: () => void;
+    let program1 = () => basic.showString("Empty");
+    let program2 = () => basic.showString("Empty");
+    let program3 = () => basic.showString("Empty");
+    let program4 = () => basic.showString("Empty");
+    let program5 = () => basic.showString("Empty");
 
     /**
      * A simple event taking a function handler
@@ -14,6 +16,8 @@ namespace cyberbot{
         if (menuItem === BotMenu.Program1){program1 = customFunction;}
         if (menuItem === BotMenu.Program2){program2 = customFunction;}
         if (menuItem === BotMenu.Program3){program3 = customFunction;}
+        if (menuItem === BotMenu.Program4){program4 = customFunction;}
+        if (menuItem === BotMenu.Program5){program5 = customFunction;}
     }
 
     //% block="execute %menuItem"
@@ -21,6 +25,35 @@ namespace cyberbot{
         if (menuItem === BotMenu.Program1){program1();}
         if (menuItem === BotMenu.Program2){program2();}
         if (menuItem === BotMenu.Program3){program3();}
+        if (menuItem === BotMenu.Program4){program4();}
+        if (menuItem === BotMenu.Program5){program5();}
+    }
+
+    //% block="use program menu"
+    export function useProgramMenu(): void{
+        let selection = 0
+        led.plot(selection, 2)
+       
+            input.onButtonPressed(Button.A, function () {
+                selection -= 1;
+                if (selection < 0) {selection = 0}
+                basic.clearScreen()
+                led.plot(selection, 2)
+            })
+            input.onButtonPressed(Button.B, function () {
+                selection += 1;
+                if (selection > 4) {selection = 4}
+                basic.clearScreen()
+                led.plot(selection, 2)
+            })
+            input.onButtonPressed(Button.AB, function () {
+                basic.clearScreen();
+                execute(selection);
+                basic.clearScreen();
+                led.plot(selection, 2)
+            })
+      
+
     }
 
 }
@@ -28,5 +61,7 @@ namespace cyberbot{
 enum BotMenu{
     Program1,
     Program2,
-    Program3
+    Program3,
+    Program4,
+    Program5
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 cyberbot.setProgram(BotMenu.Program2, function () {
     basic.showIcon(IconNames.Surprised)
+    basic.pause(1000)
 })
 cyberbot.setProgram(BotMenu.Program1, function () {
     basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
 })
-cyberbot.execute(BotMenu.Program2)
+cyberbot.useProgramMenu()
